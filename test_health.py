e01775fbfc04ff88d6e6d463f28a6e0890bdcc4b
LucasTCamp <lucastcampbell@outlook.com>
2026-03-26 07:55:14 -0400

    Refactor imports in test_health.py

--- test_health.py
+++ test_health.py
@@ -1,13 +1,16 @@
-from health import *
+from health import take_damage, heal
+
 
 def test_take_damage(player):
     result = take_damage(player, 30)
     assert result["health"] == 40
 
+
 def test_heal_adds_health(player):
     result = heal(player, 30)
     assert result["health"] == 100
 
+
 def test_is_alive_true(player):
     result = take_damage(player, 100)
-    assert result["alive"] == False
+    assert result["alive"] is False
